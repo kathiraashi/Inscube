d98d63e2a33284531f5abb0864077cf7842b29ee
kathiraashi <kathiraashi@gmail.com>
2018-11-02 14:59:22 +0530

    SSL Bug Fixed

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var cors = require('cors');
 var ErrorManagement = require('./app/config/ErrorHandling.js');
 var parser = require('ua-parser-js');
 
+// const fs = require('fs');
+// const http = require('http');
+// const https = require('https');
+
 var admin = require('firebase-admin');
 var serviceAccount = require('./inscube_firebase_admin.json');
 
@@ -14,6 +18,17 @@ var serviceAccount = require('./inscube_firebase_admin.json');
 
 var port = process.env.PORT || 4000;
 var app = express();
+
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/inscube.com/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/inscube.com/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/inscube.com/chain.pem', 'utf8');
+
+// const credentials = {
+// 	key: privateKey,
+// 	cert: certificate,
+// 	ca: ca
+// };
+
 
 
 // Process On Every Error
@@ -30,9 +45,8 @@ var app = express();
 
 
 // DB Connection
-//
-   //  mongoose.connect('mongodb://localhost/Inscube');
     mongoose.connect('mongodb://kathiravan:kathir143@ds241699.mlab.com:41699/inscube');
+    //mongoose.connect('mongodb://kathiravan:kathir143@localhost/Inscube');
     mongoose.connection.on('error', function(err) {
         ErrorManagement.ErrorHandling.ErrorLogCreation('', 'Mongodb Connection Error', 'Server.js - 31', err);
     });
@@ -59,13 +73,31 @@ app.use('/API/Uploads', express.static('Uploads'));
 
     require('./app/routes/Trends.routes.js')(app);
 
-// app.get('*', function(req, res, next){
-//     var DeviceInfo = parser(req.headers['user-agent']);
-//     if(DeviceInfo.os.name === 'Android') {
-//         res.redirect(301, 'https://play.google.com/store/apps/details?id=com.test.aashi.inscubenewbuild' );
-//     } else {
-//         next();
-//     }
+//     app.get('*', function(req, res, next){
+//       var DeviceInfo = parser(req.headers['user-agent']);
+//       if(DeviceInfo.os.name === 'Android') {
+//           res.redirect(301, 'https://play.google.com/store/apps/details?id=com.test.aashi.inscubenewbuild' );
+//       } else {
+//           next();
+//       }
+//   });
+
+
+// app.use(express.static(__dirname + '/view/dist/'));
+
+// app.use(function(req, res) {
+//      res.sendFile(path.join(__dirname, '/view/dist', 'index.html'));
+// });
+
+// http.createServer(function (req, res) {
+//    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+//    res.end();
+// }).listen(80);
+
+// const httpsServer = https.createServer(credentials, app);
+
+// httpsServer.listen(443, () => {
+// 	console.log('HTTPS Server running on port 443');
 // });
 
 
